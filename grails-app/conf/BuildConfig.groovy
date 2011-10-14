ea924bd2dbbbb575752aee3ccc068ab02dc71dd8
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2011-10-14 01:01:36 +0100

    Clean build configuration file.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -4,24 +4,24 @@ grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
-        // excludes 'ehcache'
     }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
+
+	log 'warn'
+
+	repositories {
 		grailsPlugins()
 		grailsHome()
 		grailsCentral()
-        mavenCentral()
-        mavenLocal()
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-	compile ('org.springframework.security.extensions:spring-security-saml2-core:1.0.0-RC1-SNAPSHOT') {
-		excludes "xml-apis", "commons-logging","slf4j-api", "jul-to-slf4j"
+
+		mavenCentral()
 	}
+
+    dependencies {
+		compile ('org.springframework.security.extensions:spring-security-saml2-core:1.0.0-RC1-SNAPSHOT') {
+			excludes "xml-apis", "commons-logging","slf4j-api", "jul-to-slf4j"
+		}
     }
 
     plugins {

b3fd95583a8e804fa78155f0df8e30b56290cc33
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2011-10-19 10:57:42 +0100

    Update dependencies to isolate required libraries to minimise application dependency clashes.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -6,10 +6,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.release.scm.enabled = false
 
 grails.project.dependency.resolution = {
-    inherits("global") {
-        // uncomment to disable ehcache
-    }
-
+    inherits 'global'
 	log 'warn'
 
 	repositories {
@@ -22,8 +19,38 @@ grails.project.dependency.resolution = {
 
     dependencies {
 		compile ('org.springframework.security.extensions:spring-security-saml2-core:1.0.0-RC1-SNAPSHOT') {
-			excludes "xml-apis", "commons-logging","slf4j-api", "jul-to-slf4j"
+			transitive = false
 		}
+		compile ('org.opensaml:opensaml:2.4.1') {
+			transitive = false
+		}
+		compile ('org.opensaml:xmltooling:1.3.1') {
+			transitive = false
+		}
+		compile ('org.apache.santuario:xmlsec:1.4.4') {
+			transitive = false
+		}	
+		compile ('org.bouncycastle:bcprov-jdk15:1.45') {
+			transitive = false
+		}
+		compile ('org.opensaml:openws:1.4.1') {
+			transitive = false
+		}
+		compile('joda-time:joda-time:1.6.2') {
+			transitive = false
+		}
+		compile('commons-httpclient:commons-httpclient:3.1') {
+			transitive = false
+		}
+		compile('org.apache.velocity:velocity:1.7') {
+			transitive = false
+		}
+		compile ('ca.juliusdavies:not-yet-commons-ssl:0.3.9') {
+			transitive = false 
+		}
+		/*compile('xerces:xercesImpl:2.8.1') {
+			transitive = false
+		}*/
     }
 
     plugins {

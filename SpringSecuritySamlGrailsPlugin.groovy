48a50c107b437d72d9e008df5595ca356e5b7af3
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2011-10-20 15:07:58 +0100

    Update version to 1.0.0M3 changes to improves configurability.

--- SpringSecuritySamlGrailsPlugin.groovy
+++ SpringSecuritySamlGrailsPlugin.groovy
@@ -46,7 +46,7 @@ import org.apache.commons.httpclient.HttpClient
 
 class SpringSecuritySamlGrailsPlugin {
     // the plugin version
-    def version = "1.0.0.M2"
+    def version = "1.0.0.M3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on

3b99fb0d8843c0350ef7e33fa88c3430c7e4a98b
Feroz Panwaskar <feroz.panwaskar@gmail.com>
2012-01-31 00:10:14 +0000

    Release version 1.0.0.M13 - Fix bugs with transitive dependencies

--- SpringSecuritySamlGrailsPlugin.groovy
+++ SpringSecuritySamlGrailsPlugin.groovy
@@ -49,7 +49,7 @@ import org.apache.commons.httpclient.HttpClient
 
 class SpringSecuritySamlGrailsPlugin {
     // the plugin version
-    def version = "1.0.0.M12"
+    def version = "1.0.0.M13"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
